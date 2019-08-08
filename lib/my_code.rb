6704657c6b1a5e382a436b2e5a20b01f838a7164
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-08 14:25:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,7 @@ def map(source_array)
   i = 0
   while i < source_array.length do
     new.push(yield (source_array[i]))     
+      #=> will substitute block in for "yield"
     i += 1
   end
   return new
@@ -11,8 +12,12 @@ end
 def reduce(source_array, starting_point = nil, &block)
   if starting_point.nil?
     source_array.reduce(&block)
+    
+      #=> will just execute block if no starting_point provided
   else
     source_array.reduce(starting_point, &block)
+    
+      #=> will execute block with starting_point value provided
   end
 end
 
